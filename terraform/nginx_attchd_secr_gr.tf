80536e0ea4e940daf88b72a006e4ec1904b5c8da
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2020-11-29 14:26:32 +0300

    Create nginx_attchd_secr_gr.tf

---
+++ terraform/nginx_attchd_secr_gr.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "XXXXXXXX"
+  secret_key = "XXXXXXXX"
+}
+
+
+resource "aws_security_group" "nginx_prod" {
+  name 	      = "nginx"
+  description = "Allow http inbout and outbound"
+
+  ingress{
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["XXXXXX/32"]
+  }
+  ingress{
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["XXXXXX/32"]
+  }
+  egress{
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["XXXXXX/32"]
+  }
+  tags = {
+    "Terraform" : "true"
+  }
+}
+resource "aws_instance" "prod_web_nginx" {
+  ami           = "ami-ID"
+  instance_type = "t2.nano"
+
+  vpc_security_group_ids = [
+    aws_security_group.nginx_prod.id
+  ]
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+
